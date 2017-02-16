ccdc4d5cb01d273802775f2e2f54e31b17b3228d
Edwar Baron <edwar.baron@gmail.com>
2017-02-15 21:20:57 -0500

    (labcomp-api) Update requeriments

--- django/labcomp/urls.py
+++ django/labcomp/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_swagger_view(title='Pastebin API')
+schema_view = get_swagger_view(title='Labcomp API')
 
 urlpatterns = [
     url(r'^$', schema_view),

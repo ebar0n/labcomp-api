4465faebcab9065fcc653f7082e5153e80d5b950
lzambrano18 <lezy17@gmail.com>
2017-02-19 02:20:53 -0400

    New struct base

--- django/lab_reservations/api/timetable_api.py
+++ django/lab_reservations/api/timetable_api.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 
 from lab_reservations.models import CHOICES_BLOCKS, CHOICES_DAYS, Reservation, TimeTable
 from lab_reservations.serializers import ReservationSerializer, RoomTimeTableSerializer, TimeTableSerializer
-from lab_rooms.models import Room
+from lab_rooms.models import Room, RoomCharacteristic, TypeCharacteristic, TypeInfrastructure
 
 
 class TimeTableViewSet(viewsets.ModelViewSet):
@@ -28,10 +28,36 @@ class BaseTimeTableView(views.APIView):
             obj[0]: obj[1]
             for obj in CHOICES_DAYS
         }
-        base['rooms'] = {
-            obj.pk: obj.name
-            for obj in Room.objects.all()
-        }
+
+        json = {}
+        types = TypeInfrastructure.objects.all()
+        for type in types:
+            json[type.id] = {}
+            json[type.id]['name'] = type.name
+            json[type.id]['icon'] = type.icon
+            json[type.id]['rooms'] = {}
+            for room in Room.objects.filter(type=type.id):
+                json[type.id]['rooms'][room.id] = {}
+                json[type.id]['rooms'][room.id]['name'] = room.name
+                json[type.id]['rooms'][room.id]['characteristics'] = []
+                for typesc in TypeCharacteristic.objects.all():
+                    json1 = {}
+                    json1[typesc.name] = {}
+                    json1[typesc.name]['icon'] = typesc.icon
+                    characteristics = RoomCharacteristic.objects.filter(
+                        characteristic__type=typesc.id,
+                        room=room.id)
+                    arrayObj = []
+                    for characteristic in characteristics:
+                        json2 = {}
+                        json2[characteristic.characteristic.name] = {}
+                        json2[characteristic.characteristic.name]['icon'] = characteristic.characteristic.icon
+                        json2[characteristic.characteristic.name]['value'] = characteristic.value
+                        arrayObj.append(json2)
+                    json1[typesc.name]['characteristics'] = arrayObj
+                    json[type.id]['rooms'][room.id]['characteristics'].append(json1)
+
+        base['infrastructures'] = json
         return Response(base)
 
 
